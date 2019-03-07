4ba16fd3333ef428c2e6270cf30d96e60b202ad9
sandd95 <sandeepshah1995@gmail.com>
2019-03-07 13:27:46 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## this is a test magento 2 module
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sandeep_Magento2Module',
+    __DIR__
+);
